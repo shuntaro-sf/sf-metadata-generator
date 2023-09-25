48696e1ca1294e952a6188963b0f5c06253084c9
usagi-san-dayo <plain.1998.shun@gmail.com>
2023-09-26 00:18:36 +0900

    fix: test

--- test/commands/metadata/field/metadata_field_generateTemplate.nut.ts
+++ test/commands/metadata/field/metadata_field_generateTemplate.nut.ts
@@ -2,23 +2,17 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable no-console */
-import { execCmd, TestSession } from '@salesforce/cli-plugins-testkit';
+import { execCmd } from '@salesforce/cli-plugins-testkit';
 import { FieldGenerateResult } from '../../../../src/commands/metadata/field/generate';
 import { FieldTemplateResult } from '../../../../src/commands/metadata/field/template';
 
 const outputDir = './test/resources/project/force-app/main/default/objects/Account/fields/';
 const templateOutputDir = './test/resources/input/field/';
 
-let testSession: TestSession;
-
 describe('metadata field generate template NUTs', () => {
-  before('prepare session', async () => {
-    testSession = await TestSession.create();
-  });
+  before('prepare session', async () => {});
 
-  after(async () => {
-    await testSession?.clean();
-  });
+  after(async () => {});
 
   it('metadata field generate', () => {
     execCmd<FieldTemplateResult>('metadata field template --outputdir ' + templateOutputDir + ' --json', {
